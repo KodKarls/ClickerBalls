05b91d965e44d14856ad6489ad538fd0c241d203
Carolinii <carolinii.contact@gmail.com>
2022-03-08 13:29:00 +0100

    add settings

---
+++ conf.lua
@@ -0,0 +1,8 @@
+-- Game settings.
+function love.conf(t)
+    t.version = "11.4"                              -- The LÖVE version this game was made for (string)
+    t.window.title = "Clicker Balls"                -- The window title (string)
+    t.window.icon = "res/images/balls-icon.jpg"     -- Filepath to an image to use as the window's icon (string)
+    t.window.width = 800                            -- The window width (number)
+    t.window.height = 600                           -- The window height (number)
+end
